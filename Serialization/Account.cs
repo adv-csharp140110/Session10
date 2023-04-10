ef1d1210f56bf2b7050dd82bcc18572ed27ed675
Pasha <pashaie@gmail.com>
2023-04-10 19:13:57 +0430

    Add project files.

---
+++ Serialization/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long Balnce { get; set; }
+    }
+}
---
+++ Serialization/Program.cs
@@ -0,0 +1,70 @@
+﻿using Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+var account = new Account {Id = 1, Name = "Central", Balnce = 50000 };
+
+
+/*  Serialize
+ *      Plain Text
+ *          XML
+ *                  <Acccount>
+                        <Id>2</Id>
+                           ...
+                    </Acccount>
+              SOAP
+ *          JSON - 
+ *                {
+ *                  "Id" : 2,
+ *                  "Name", "Central",
+ *                  "Balance": 50000
+ *                }  
+ *                  
+ *                  
+ *          YAML
+ *          TOML - ini
+ *          ...
+ *      Binary
+ *          c#
+            ProtoBuf
+
+
+ */
+
+//XML
+var xmlSer = new XmlSerializer(typeof(Account));
+
+//XML Serialize
+var xmlWriter = new StreamWriter(AppContext.BaseDirectory + "/account.xml");
+xmlSer.Serialize(xmlWriter, account);
+xmlWriter.Close();
+Console.WriteLine("XML Serialize");
+
+
+//XML Deserialize
+var xmlReader = new StreamReader(AppContext.BaseDirectory + "/account.xml");
+var accountDesr = xmlSer.Deserialize(xmlReader) as Account;
+Console.WriteLine($"XML Deserialize {accountDesr.Id} - {accountDesr.Balnce}");
+
+
+
+//JSOM
+// NewtonSoft
+// System.Text.JSON 💘
+var json = JsonSerializer.Serialize(account);
+File.WriteAllText(AppContext.BaseDirectory + "/account.json", json);
+Console.WriteLine("JSON Serialize");
+
+var accountDesrJSON = JsonSerializer.Deserialize<Account>(json);
+Console.WriteLine($"JSON Deserialize {accountDesrJSON.Id} - {accountDesrJSON.Balnce}");
+
+
+#pragma warning disable SYSLIB0011
+//Binary c#
+var binaryFormatter = new BinaryFormatter();
+var binWriter = new FileStream(AppContext.BaseDirectory + "/account.bin", FileMode.Create);
+binaryFormatter.Serialize(binWriter, account);
+binWriter.Close();
+
+Console.ReadLine();
